74dec59441f0c1fabdb5ccd3094e9e2bf2072b12
Ian <fishy90@gmail.com>
2020-07-24 00:06:52 +0800

    add images to show load more button functionality

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,37 @@
 $(document).ready(function () {
+  const itemList = [
+    { fileName: "topselling-09", description: "Long red dress" },
+    { fileName: "topselling-10", description: "Pinstripe wide trousers" },
+    { fileName: "topselling-11", description: "B&W polkadot dress" },
+    {
+      fileName: "topselling-12",
+      description: "Sheer sleeved blouse",
+    },
+    { fileName: "topselling-13", description: "White collared blouse" },
+    { fileName: "topselling-14", description: "Long deep blue skirt" },
+    { fileName: "topselling-15", description: "Burgundy sweater" },
+  ];
   $(".load-more").click(function (e) {
     console.log(e);
+
+    for (let i = 0; i < 4; i++) {
+      let item = itemList.shift();
+      $("#top-selling-products .product-list")
+        .append(`<div class="col-6 col-md-3">
+      <span class="store-buttons">
+        <button class="btn btn-light rounded-circle px-2 py-1 mr-2" type="button">
+          <span class="icon-shopping-bag"></span>
+        </button>
+        <button class="btn btn-light rounded-circle px-2 py-1" type="button"><span
+            class="icon-heart"></span></button>
+      </span>
+      <img class="img-fluid" src="./img/${item.fileName}.jpg">
+      <div class="description d-flex flex-row justify-content-between">
+        <span class="item-name">${item.description}</span>
+        <span class="price">$35.00</span>
+      </div>
+    </div>`);
+      console.log(itemList);
+    }
   });
 });
